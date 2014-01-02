8226b8f7464b57edb5bcf54de1c090c3de0c9bf3
Amit Dhara <amit.dhara@ctb.com>
2014-01-02 13:01:10 +0000

    Amitabha: Code formatting, comments, etc.

--- INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/business/IUsabilityBusiness.java
+++ INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/business/IUsabilityBusiness.java
@@ -2,19 +2,52 @@
  * 
  */
 package com.ctb.prism.core.business;
+
 import com.ctb.prism.core.transferobject.UsabilityTO;
 import com.ctb.prism.webservice.transferobject.StudentDataLoadTO;
 import com.ctb.prism.webservice.transferobject.StudentListTO;
 
 /**
+ * Business to report usability
+ * 
  * @author d-abir_dutta
- *  Business to report usability
  */
 public interface IUsabilityBusiness {
 
+	/**
+	 * @param usability
+	 * @return
+	 * @throws Exception
+	 */
 	public boolean saveUsabilityData(UsabilityTO usability) throws Exception;
+
+	/**
+	 * Update webservice data to staging.
+	 * 
+	 * @param studentListTO
+	 * @param studentDataLoadTO
+	 * @return
+	 * @throws Exception
+	 */
 	public StudentDataLoadTO updateStagingData(StudentListTO studentListTO, StudentDataLoadTO studentDataLoadTO) throws Exception;
+
+	/**
+	 * @param partitionName
+	 * @throws Exception
+	 */
 	public void updatePartition(String partitionName) throws Exception;
+
+	/**
+	 * @return
+	 * @throws Exception
+	 */
 	public String checkPartition() throws Exception;
+
+	/**
+	 * @param studentListTO
+	 * @param studentDataLoadTO
+	 * @return
+	 * @throws Exception
+	 */
 	public StudentDataLoadTO createProces(StudentListTO studentListTO, StudentDataLoadTO studentDataLoadTO) throws Exception;
 }
--- INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/business/UsabilityBusinessImpl.java
+++ INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/business/UsabilityBusinessImpl.java
@@ -14,19 +14,23 @@ import com.ctb.prism.webservice.transferobject.StudentDataLoadTO;
 import com.ctb.prism.webservice.transferobject.StudentListTO;
 
 /**
- * @author d-abir_dutta
  * Service to report usability
- *
+ * 
+ * @author d-abir_dutta
+ * 
  */
 
 @Component
 public class UsabilityBusinessImpl implements IUsabilityBusiness {
 
 	private static final IAppLogger logger = LogFactory.getLoggerInstance(UsabilityBusinessImpl.class.getName());
-	
+
 	@Autowired
 	IUsabilityDAO usabilityDAO;
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.ctb.prism.core.business.IUsabilityBusiness#saveUsabilityData(com.ctb.prism.core.transferobject.UsabilityTO)
 	 */
 	public boolean saveUsabilityData(UsabilityTO usability) throws Exception {
@@ -34,30 +38,46 @@ public class UsabilityBusinessImpl implements IUsabilityBusiness {
 		logger.log(IAppLogger.INFO, "UsabilityBusinessImpl:saveUsabilityData");
 		return usabilityDAO.saveUsabilityData(usability);
 	}
-	
-	/**
-	 * Update webservice data to staging
-	 * @param studentListTO
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.ctb.prism.core.business.IUsabilityBusiness#updateStagingData(com.ctb.prism.webservice.transferobject.StudentListTO, com.ctb.prism.webservice.transferobject.StudentDataLoadTO)
 	 */
 	public StudentDataLoadTO updateStagingData(StudentListTO studentListTO, StudentDataLoadTO studentDataLoadTO) throws Exception {
 		studentDataLoadTO.setPartitionName(studentDataLoadTO.getPartitionName());
 		// create process id with this partition - moved to controller
 		// studentDataLoadTO = usabilityDAO.createProces(studentListTO, studentDataLoadTO);
-		if(studentDataLoadTO.getProcessId() != 0) {
+		if (studentDataLoadTO.getProcessId() != 0) {
 			usabilityDAO.updateStagingData(studentListTO, studentDataLoadTO);
 		}
-	
+
 		return studentDataLoadTO;
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.ctb.prism.core.business.IUsabilityBusiness#updatePartition(java.lang.String)
+	 */
 	public void updatePartition(String partitionName) throws Exception {
 		usabilityDAO.updatePartition(partitionName);
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.ctb.prism.core.business.IUsabilityBusiness#checkPartition()
+	 */
 	public String checkPartition() throws Exception {
 		return usabilityDAO.checkPartition();
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.ctb.prism.core.business.IUsabilityBusiness#createProces(com.ctb.prism.webservice.transferobject.StudentListTO, com.ctb.prism.webservice.transferobject.StudentDataLoadTO)
+	 */
 	public StudentDataLoadTO createProces(StudentListTO studentListTO, StudentDataLoadTO studentDataLoadTO) throws Exception {
 		return usabilityDAO.createProces(studentListTO, studentDataLoadTO);
 	}
