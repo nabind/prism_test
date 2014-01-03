bc004c6a5d92115019dd1be4d0363c964b5e0fd4
Amit Dhara <amit.dhara@ctb.com>
2014-01-03 10:32:03 +0000

    Parent Network - By Joy

--- INORS/Codebase/INORS/prism-web/src/main/webapp/scripts/js/parent/parentNetwork.js
+++ INORS/Codebase/INORS/prism-web/src/main/webapp/scripts/js/parent/parentNetwork.js
@@ -6,8 +6,47 @@
 $(document).ready(function() {
 	showContent($('#studentOverviewMessage'));
 	showContent($('#contentDescription'));
+	
+	//========= Function to get standards pages =====
+	$(".activitydata").live('click', function() {
+		getStudentPage('getStandardActivity.do', $(this));
+	});
+	$(".standardsdata").live('click', function() {
+		getStudentPage('getStandardIndicator.do', $(this));
+	});
+	$(".articledata").live('click', function() {
+		getStudentPage('getArticleDescription.do', $(this));
+	});
+	
+	
 });
 //=====document.ready End===================================
+
+//======== Function to get standards pages =================
+function getStudentPage(urlParam, obj) {
+	blockUI();
+	var dataUrl = 'subtestId='+$(obj).attr('subtestId')
+					+'&studentBioId='+$(obj).attr('studentBioId')
+					+'&studentName='+$(obj).attr('studentName')
+					+'&studentGradeName='+$(obj).attr('studentGradeName')
+					+'&studentGradeId='+$(obj).attr('studentGradeId')
+					+'&articleId='+$(obj).attr('articleId')
+					+'&contentType='+$(obj).attr('contentType');
+	 $.ajax({
+		type : "GET",
+		url : urlParam,
+		data : dataUrl,
+		dataType : 'html',
+		cache:false,
+		success : function(data) {
+			unblockUI();
+			$(".main-section").html(data);
+		},
+		error : function(data) {						
+			unblockUI();
+		}
+	});
+}
 
 //============To show dynamic content in HTML===============
 function showContent($container){
