96ddd5efc7f36d0325f90bfd5ed9d8c3941d5e39
Amit Dhara <amit.dhara@ctb.com>
2014-03-19 06:27:20 +0000

    Amitabha: Formatted commented codes only

--- INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/util/Utils.java
+++ INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/util/Utils.java
@@ -103,8 +103,14 @@ public final class Utils {
 		// String pattern ="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
 		String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,}$";
 		/**
-		 * ^ # start-of-string (?=.*[0-9]) # a digit must occur at least once (?=.*[a-z]) # a lower case letter must occur at least once (?=.*[A-Z]) # an upper case letter must occur at least once
-		 * (?=.*[@#$%^&+=]) # a special character must occur at least once (?=\S+$) # no whitespace allowed in the entire string .{8,} # anything, at least eight places though $ # end-of-string
+     	 ^                # start-of-string
+		(?=.*[0-9])       # a digit must occur at least once
+		(?=.*[a-z])       # a lower case letter must occur at least once
+		(?=.*[A-Z])       # an upper case letter must occur at least once
+		(?=.*[@#$%^&+=])  # a special character must occur at least once
+		(?=\S+$)          # no whitespace allowed in the entire string
+		.{8,}             # anything, at least eight places though
+		$                 # end-of-string
 		 */
 
 		return (passwd.matches(pattern));
