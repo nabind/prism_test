701ad9597f9f28c288516e724179b4f69661cc48
Amit Dhara <amit.dhara@ctb.com>
2014-03-21 04:27:52 +0000

    Amitabha: dataload message

--- INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/constant/IQueryConstants.java
+++ INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/constant/IQueryConstants.java
@@ -50,7 +50,8 @@ public interface IQueryConstants extends IUserQuery, IOrgQuery, IParentQuery, IR
 			" WHERE DR.DB_REPORTID = ?",
 			" AND DM.ACTIVATION_STATUS = 'AC' AND DM.CUST_PROD_ID = DMT.CUST_PROD_ID",
 			" AND DM.CUST_PROD_ID = (SELECT CUST_PROD_ID FROM CUST_PRODUCT_LINK WHERE PRODUCTID = ? AND CUSTOMERID = ?)",
-			" AND DMRA.ORG_LEVEL = ?"
+			" AND DMRA.ORG_LEVEL = ?",
+			" ORDER BY DMT.MESSAGE_TYPE"
 			);
 	
 	// query to retrieve tenant id for a particular username
