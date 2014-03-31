d5a64952df4d5a577b5c14e0c5e1d0f683c972da
Amit Dhara <amit.dhara@ctb.com>
2014-03-31 13:23:47 +0000

    Amitabha: QC #77855

--- INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/util/FileUtil.java
+++ INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/util/FileUtil.java
@@ -592,6 +592,9 @@ public class FileUtil {
 	public static String generateDefaultZipFileName(String currentUser, String groupFile) {
 		String zipFileName = "";
 		String userName = currentUser.replaceAll("[^a-zA-Z0-9 ._-]", "");
+		if (userName.length() > 10){
+			userName = userName.substring(0, 10);
+		}
 		// if(userName.indexOf("9rc^w") != -1) userName = userName.substring(0, userName.indexOf("9rc^w"));
 		if (IApplicationConstants.EXTRACT_FILETYPE.ICL.toString().equals(groupFile)) {
 			zipFileName = CustomStringUtil.appendString(userName, " ", CustomStringUtil.replaceAll(Utils.getDateTime(), "-", "."), " ", groupFile);
