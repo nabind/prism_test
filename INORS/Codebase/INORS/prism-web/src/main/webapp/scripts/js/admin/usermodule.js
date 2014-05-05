9778a3e8a0199629a934005b2c932b9f05ca58f6
Amit Dhara <amit.dhara@ctb.com>
2014-05-05 22:15:45 +0000

    *** empty log message ***

--- INORS/Codebase/INORS/prism-web/src/main/webapp/scripts/js/admin/usermodule.js
+++ INORS/Codebase/INORS/prism-web/src/main/webapp/scripts/js/admin/usermodule.js
@@ -286,7 +286,7 @@
 									// END : open tree on clicking # of users in org screen
 									
 									//Code to select the first node on load
-									if((redirectLevel == 1 || redirectLevel == 0) || objectTree == null) {
+									if((redirectLevel == 1 || redirectLevel == 0) || objectTree == null || tempTree == null) {
 										selectTheFirstNodeOfTree($("#treeViewForOrg > ul > li:first").attr("id"));
 									}
 									if($("#isRedirectedTree").val()=='Yes'){
