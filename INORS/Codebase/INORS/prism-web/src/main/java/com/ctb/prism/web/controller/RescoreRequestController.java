d263c3d365436272701abbf75cde6268648e036a
Amit Dhara <amit.dhara@ctb.com>
2014-05-15 04:54:56 +0000

    Amitabha: Report Messages

--- INORS/Codebase/INORS/prism-web/src/main/java/com/ctb/prism/web/controller/RescoreRequestController.java
+++ INORS/Codebase/INORS/prism-web/src/main/java/com/ctb/prism/web/controller/RescoreRequestController.java
@@ -10,26 +10,25 @@ import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 import com.ctb.prism.core.constant.IApplicationConstants;
 import com.ctb.prism.core.exception.BusinessException;
 import com.ctb.prism.core.logger.IAppLogger;
 import com.ctb.prism.core.logger.LogFactory;
 import com.ctb.prism.core.resourceloader.IPropertyLookup;
 import com.ctb.prism.core.util.CustomStringUtil;
-import com.ctb.prism.core.util.GenericStack;
 import com.ctb.prism.core.util.Utils;
 import com.ctb.prism.login.transferobject.UserTO;
 import com.ctb.prism.report.service.IReportService;
 import com.ctb.prism.report.service.IRescoreRequestService;
-import com.ctb.prism.report.transferobject.InputControlTO;
+import com.ctb.prism.report.transferobject.ReportMessageTO;
 import com.ctb.prism.report.transferobject.RescoreRequestTO;
- 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Joy
@@ -87,6 +86,7 @@ public class RescoreRequestController {
 		logger.log(IAppLogger.INFO, "grade=" + grade);
 		logger.log(IAppLogger.INFO, "reportUrl=" + reportUrl);
 		
+		
 		try{
 			if ((testAdministrationVal == null) || ("null".equalsIgnoreCase(testAdministrationVal))) {
 				Map<String, Object> parameters = inorsController.getReportParameters(request, reportUrl);
@@ -143,7 +143,41 @@ public class RescoreRequestController {
 			long t2 = System.currentTimeMillis();
 			logger.log(IAppLogger.INFO, "Exit: RescoreRequestController - rescoreRequestForm() took time: "+String.valueOf(t2 - t1)+"ms");
 		}
+		
+		// String reportId = (String) request.getParameter("reportId");
+		String productId = (testAdministrationVal == null) ? IApplicationConstants.DEFAULT_PRODUCT_ID : testAdministrationVal;
+		String customerId = (String) request.getSession().getAttribute(IApplicationConstants.CUSTOMER);
+		String orgNodeLevel = ((Long) request.getSession().getAttribute(IApplicationConstants.CURRORGLVL)).toString();
+		String currentUserId = (String) request.getSession().getAttribute(IApplicationConstants.CURRUSERID);
+		logger.log(IAppLogger.INFO, "reportId=" + reportId);
+		logger.log(IAppLogger.INFO, "productId=" + productId);
+		logger.log(IAppLogger.INFO, "customerId=" + customerId);
+		logger.log(IAppLogger.INFO, "orgNodeLevel=" + orgNodeLevel);
+		Map<String, Object> messageParamMap = new HashMap<String, Object>();
+		messageParamMap.put("REPORT_ID", reportId);
+		messageParamMap.put("PRODUCT_ID", productId);
+		messageParamMap.put("CUSTOMER_ID", customerId);
+		messageParamMap.put("ORG_NODE_LEVEL", orgNodeLevel);
+		messageParamMap.put("USER_ID", currentUserId);
+		List<ReportMessageTO> reportMessages = reportService.getAllReportMessages(messageParamMap);
+		modelAndView.addObject("reportMessages", reportMessages);
+		String dataloadMessage = getReportMessage(reportMessages, IApplicationConstants.DASH_MESSAGE_TYPE.DM.toString(), IApplicationConstants.DATALOAD_MESSAGE);
+		logger.log(IAppLogger.INFO, "dataloadMessage=" + dataloadMessage);
+		modelAndView.addObject("dataloadMessage", dataloadMessage);
 		return modelAndView;
+	}
+	
+	private String getReportMessage(List<ReportMessageTO> reportMessages, String messageType, String messageName) {
+		String message = null;
+		if ((reportMessages != null) && (!reportMessages.isEmpty())) {
+			for (ReportMessageTO reportMessage : reportMessages) {
+				if ((messageType.equals(reportMessage.getMessageType())) && (messageName.equals(reportMessage.getMessageName()))) {
+					message = reportMessage.getMessage();
+					break;
+				}
+			}
+		}
+		return message;
 	}
 
 }
