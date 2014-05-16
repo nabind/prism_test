593d710f1b5a5438e6b96b2ab61911392f7a6388
Amit Dhara <amit.dhara@ctb.com>
2014-05-16 10:11:14 +0000

    Joy: Remove "Standard x -"

--- INORS/Database_Scripts/Operation_Schema/AlterScript/Objective_desc.sql
+++ INORS/Database_Scripts/Operation_Schema/AlterScript/Objective_desc.sql
@@ -14,131 +14,131 @@ ALTER TABLE OBJECTIVE_DIM RENAME COLUMN DATETIMESTAMP1 TO DATETIMESTAMP;
 --For Science
 
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 1-The Nature of Science and Technology'
+   SET OBJECTIVE_DESC = 'The Nature of Science and Technology'
  WHERE objective_name = 'Nature of Sci & Tech';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 2-Scientific Thinking'
+   SET OBJECTIVE_DESC = 'Scientific Thinking'
  WHERE objective_name = 'Scientific Thinking';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 3-The Physical Setting'
+   SET OBJECTIVE_DESC = 'The Physical Setting'
  WHERE objective_name = 'The Physical Setting';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 4-The Living Environment'
+   SET OBJECTIVE_DESC = 'The Living Environment'
  WHERE objective_name = 'The Living Environment';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 5-The Mathematical World'
+   SET OBJECTIVE_DESC = 'The Mathematical World'
  WHERE objective_name = 'The Mathematical World';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 7-Common Themes'
+   SET OBJECTIVE_DESC = 'Common Themes'
  WHERE objective_name = 'Common Themes'; 
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 2 - Earth Science'
+   SET OBJECTIVE_DESC = 'Earth Science'
  WHERE objective_name = 'Earth Science'; 
  
 -- Grade 6 Science
 
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 1 - Physical Science'
+   SET OBJECTIVE_DESC = 'Physical Science'
  WHERE objective_name = 'Physical Science';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 2 - Earth and Space Science'
+   SET OBJECTIVE_DESC = 'Earth and Space Science'
  WHERE objective_name = 'Earth & Space Science';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 3 - Life Science'
+   SET OBJECTIVE_DESC = 'Life Science'
  WHERE objective_name = 'Life Science';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 4 - Science, Engineering and Technology'
+   SET OBJECTIVE_DESC = 'Science, Engineering and Technology'
  WHERE objective_name = 'Science Eng & Tech';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 5 - The Nature of Science'
+   SET OBJECTIVE_DESC = 'The Nature of Science'
  WHERE objective_name = 'The Nature of Science';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 6 - The Design Process'
+   SET OBJECTIVE_DESC = 'The Design Process'
  WHERE objective_name = 'The Design Process'; 
  
 --For ELA
 
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 1-Word Recognition, Fluency, and Vocabulary Development'
+   SET OBJECTIVE_DESC = 'Word Recognition, Fluency, and Vocabulary Development'
  WHERE objective_name = 'Vocabulary';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 2-Comprehension and Analysis of Nonfiction and Informational Text'
+   SET OBJECTIVE_DESC = 'Comprehension and Analysis of Nonfiction and Informational Text'
  WHERE objective_name = 'Nonfiction/Info Text†';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 3-Comprehension and Analysis of Literary Text'
+   SET OBJECTIVE_DESC = 'Comprehension and Analysis of Literary Text'
  WHERE objective_name = 'Literary Text†';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 4 WRITING: Processes and Features'
+   SET OBJECTIVE_DESC = 'WRITING: Processes and Features'
  WHERE objective_name = 'Writing Process';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 5 WRITING: Applications (Different Types of Writing and Their Characteristics)'
+   SET OBJECTIVE_DESC = 'WRITING: Applications (Different Types of Writing and Their Characteristics)'
  WHERE objective_name = 'Writing Applications';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 6 WRITING: English Language Conventions'
+   SET OBJECTIVE_DESC = 'WRITING: English Language Conventions'
  WHERE objective_name = 'Lang. Conventions'; 
 
 --For Mathematics
 
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 1-Number Sense'
+   SET OBJECTIVE_DESC = 'Number Sense'
  WHERE objective_name = 'Number Sense';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 2-Computation'
+   SET OBJECTIVE_DESC = 'Computation'
  WHERE objective_name = 'Computation';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 3-Algebra and Functions'
+   SET OBJECTIVE_DESC = 'Algebra and Functions'
  WHERE objective_name = 'Algebra & Functions';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 4-Geometry'
+   SET OBJECTIVE_DESC = 'Geometry'
  WHERE objective_name = 'Geometry';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 5-Measurement'
+   SET OBJECTIVE_DESC = 'Measurement'
  WHERE objective_name = 'Measurement';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 7-Problem Solving'
+   SET OBJECTIVE_DESC = 'Problem Solving'
  WHERE objective_name = 'Problem Solving'; 
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 6-Data Analysis and Probability'
+   SET OBJECTIVE_DESC = 'Data Analysis and Probability'
  WHERE objective_name = 'Data Analysis & Prob';  
  
  -- Social Study
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 1-History'
+   SET OBJECTIVE_DESC = 'History'
  WHERE objective_name = 'History';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 2-Civics and Government'
+   SET OBJECTIVE_DESC = 'Civics and Government'
  WHERE objective_name = 'Civics & Government'; 
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 3-Geography'
+   SET OBJECTIVE_DESC = 'Geography'
  WHERE objective_name = 'Geography';
  
 UPDATE OBJECTIVE_DIM
-   SET OBJECTIVE_DESC = 'Standard 4-Economics'
+   SET OBJECTIVE_DESC = 'Economics'
  WHERE objective_name = 'Economics'; 
  
 COMMIT;
