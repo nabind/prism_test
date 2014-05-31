e9b12b66b3dcbce3014ac3b93d1ae10d06f0c674
Amit Dhara <amit.dhara@ctb.com>
2014-05-31 08:57:41 +0000

    *** empty log message ***

--- INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/constant/IRescoreRequestQuery.java
+++ INORS/Codebase/INORS/prism-core/src/main/java/com/ctb/prism/core/constant/IRescoreRequestQuery.java
@@ -16,7 +16,7 @@ public interface IRescoreRequestQuery {
 	
 	public static final String RESET_ITEM_STATE = "PKG_RESCORE_REQUEST.SP_RESET_ITEM_STATE(?,?,?,?,?,?)";
 	
-	public static final String RESET_ITEM_DATE = "PKG_RESCORE_REQUEST.SP_RESET_ITEM_DATE(?,?,?,?)";
+	public static final String RESET_ITEM_DATE = "PKG_RESCORE_REQUEST.SP_RESET_ITEM_DATE(?,?,?,?,?)";
 	
 	public static final String GET_NOT_DNP_STUDENT = "PKG_RESCORE_REQUEST.SP_GET_NOT_DNP_STUDENT(?,?,?,?,?,?)";
 	
