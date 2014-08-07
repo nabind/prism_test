970dd0de7bebcec1ecd78820f3e281bb88be538d
Amit Dhara <amit.dhara@ctb.com>
2014-08-07 14:04:17 +0000

    *** empty log message ***

--- INORS/Database_Scripts/Operation_Schema/Growth Report/Operational Scripts/Report_Addtion_Script.sql
+++ INORS/Database_Scripts/Operation_Schema/Growth Report/Operational Scripts/Report_Addtion_Script.sql
@@ -10,31 +10,31 @@ commit;
 -- insert into DASH_MENU_RPT_ACCESS
 ------Group Performance Matrix Report
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 1, 1, 5017, 1363, 'AC', to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 1, 1, 5001, 1363, 'AC', to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'));
 
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 1, 2, 5017, 1363, 'AC', to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 1, 2, 5001, 1363, 'AC', to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'));
 
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 1, 3, 5017, 1363, 'AC', to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 1, 3, 5001, 1363, 'AC', to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:43', 'dd-mm-yyyy hh24:mi:ss'));
 
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 7, 1, 5017, 1363, 'AC', to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 7, 1, 5001, 1363, 'AC', to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'));
 
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 7, 2, 5017, 1363, 'AC', to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 7, 2, 5001, 1363, 'AC', to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'));
 
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 7, 3, 5017, 1363, 'AC', to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 7, 3, 5001, 1363, 'AC', to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:44', 'dd-mm-yyyy hh24:mi:ss'));
 
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 8, 1, 5017, 1363, 'AC', to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 8, 1, 5001, 1363, 'AC', to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'));
 
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 8, 2, 5017, 1363, 'AC', to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 8, 2, 5001, 1363, 'AC', to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'));
 
 insert into DASH_MENU_RPT_ACCESS (DB_MENUID, DB_REPORTID, ROLEID, ORG_LEVEL, CUST_PROD_ID, REPORT_SEQ, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
-values (101, 1363, 8, 3, 5017, 1363, 'AC', to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'));
+values (101, 1363, 8, 3, 5001, 1363, 'AC', to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'), to_date('30-06-2014 08:54:45', 'dd-mm-yyyy hh24:mi:ss'));
 commit;
 
 -----Student Tabular Report
