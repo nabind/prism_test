f8d839eac2bfae6002f2c5eb67f4f96763fa5c7b
Amit Dhara <amit.dhara@ctb.com>
2014-08-20 12:38:40 +0000

    Joy: Single Code base - Landing Page

---
+++ INORS/Database_Scripts/Operation_Schema/InsertScript/dash_message_type_insert.sql
@@ -0,0 +1,104 @@
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+       cust_prod_id,
+       created_date_time)
+    values
+      (1087,
+       'Common Header',
+       'GSCM',
+       'Message configuration for common header',
+       5001,
+       sysdate);
+	   
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+       cust_prod_id,
+       created_date_time)
+    values
+      (1088,
+       'Common Footer',
+       'GSCM',
+       'Message configuration for common footer',
+       5001,
+       sysdate);	   
+       
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+       cust_prod_id,
+       created_date_time)
+    values
+      (1089,
+       'Teacher Footer',
+       'GSCM',
+       'Message configuration for teacher footer',
+       5001,
+       sysdate);   
+	   
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+       cust_prod_id,
+       created_date_time)
+    values
+      (1090,
+       'Parent Footer',
+       'GSCM',
+       'Message configuration for parent footer',
+       5001,
+       sysdate);	   
+       
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+       cust_prod_id,
+       created_date_time)
+    values
+      (1091,
+       'Menu Message',
+       'GSCM',
+       'Message configuration for right sided menu',
+       5001,
+       sysdate); 
+
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+       cust_prod_id,
+       created_date_time)
+    values
+      (1092,
+       'Landing Page Content',
+       'GSCM',
+       'Image or Content for Landing page',
+       5001,
+       sysdate);
+
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+       cust_prod_id,
+       created_date_time)
+    values
+      (1093,
+       'Login Page Content',
+       'GSCM',
+       'Image or Content for Login page',
+       5001,
+       sysdate);
