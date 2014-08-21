7c4408d5a88f6ef73effa17cd7505e714c7afdf6
Abir Dutta <abir.d@tcs.com>
2014-08-21 09:21:10 +0000

    *** empty log message ***

--- INORS/Codebase/INORS/prism-web/src/main/java/com/ctb/prism/web/controller/ParentController.java
+++ INORS/Codebase/INORS/prism-web/src/main/java/com/ctb/prism/web/controller/ParentController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
