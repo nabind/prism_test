af247a40cb07952abce5cba6d869a182a2c83c7b
Amit Dhara <amit.dhara@ctb.com>
2014-08-22 08:30:50 +0000

    Amitabha: Report type changed

--- INORS/Database_Scripts/Operation_Schema/InsertScript/DASH_REPORT_insert.sql
+++ INORS/Database_Scripts/Operation_Schema/InsertScript/DASH_REPORT_insert.sql
@@ -1,24 +1,24 @@
 --1482
 insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Users','Manage Users','API','userModule.do','AC',sysdate);
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Users','Manage Users','API_LINK','userModule.do','AC',sysdate);
 --1483
 insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Organizations','Manage Organizations','API','manageOrganizations.do','AC',sysdate);
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Organizations','Manage Organizations','API_LINK','manageOrganizations.do','AC',sysdate);
 --1484
 insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Parents','Manage Parents','API','manageParent.do','AC',sysdate);
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Parents','Manage Parents','API_LINK','manageParent.do','AC',sysdate);
 --1485
 insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Students','Manage Students','API','manageStudent.do','AC',sysdate);
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Students','Manage Students','API_LINK','manageStudent.do','AC',sysdate);
 --1486
 insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Reports','Manage Reports','API','manageReports.do','AC',sysdate);
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Reports','Manage Reports','API_LINK','manageReports.do','AC',sysdate);
 --1487
 insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Content','Manage Content','API','manageContent.do','AC',sysdate);
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Content','Manage Content','API_LINK','manageContent.do','AC',sysdate);
 --1488
 insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Reset Password','Reset Password','API','resetUserPasswordForm.do','AC',sysdate);
+values(DB_REPORT_ID_SEQ.Nextval,'Reset Password','Reset Password','API_LINK','resetUserPasswordForm.do','AC',sysdate);
 --1489
 insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Education Center Users','Manage Education Center Users','API','manageEducationCenter.do','IN',sysdate);
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Education Center Users','Manage Education Center Users','API_LINK','manageEducationCenter.do','IN',sysdate);
