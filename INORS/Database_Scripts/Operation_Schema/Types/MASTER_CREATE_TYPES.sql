c63ff900e52e36983bf1a91b6b60771ca9e1905e
Amit Dhara <amit.dhara@ctb.com>
2014-08-28 06:43:00 +0000

    Joy: Single Code Base - Revert Back Array Descriptor for Manage Report

--- INORS/Database_Scripts/Operation_Schema/Types/MASTER_CREATE_TYPES.sql
+++ INORS/Database_Scripts/Operation_Schema/Types/MASTER_CREATE_TYPES.sql
@@ -4,8 +4,6 @@ prompt creating types...
 @./DYN_ATTR_DTLS.tps
 @./PRS_PGT_GLOBAL_TEMP_OBJ.typ
 @./PRS_COLL_PGT_GLOBAL_TEMP_OBJ.tps
-@./MANAGE_MESSAGE_TO_TYPE.tps
-@./MANAGE_MESSAGE_TO_ARRAY.tps
 
 
 prompt types created...
