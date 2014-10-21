167eae000be61d632c43750053b375a67a6d8699
Amit Dhara <amit.dhara@ctb.com>
2014-10-21 10:22:15 +0000

    Joy: Single Code Base - dash_reports insert

--- INORS/Database_Scripts/Operation_Schema/InsertScript/DASH_REPORT_insert.sql
+++ INORS/Database_Scripts/Operation_Schema/InsertScript/DASH_REPORT_insert.sql
@@ -1,24 +1,49 @@
---1482
-insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Users','Manage Users','API_LINK','userModule.do','AC',sysdate);
---1483
-insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Organizations','Manage Organizations','API_LINK','manageOrganizations.do','AC',sysdate);
---1484
-insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Parents','Manage Parents','API_LINK','manageParent.do','AC',sysdate);
---1485
-insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Students','Manage Students','API_LINK','manageStudent.do','AC',sysdate);
---1486
-insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Reports','Manage Reports','API_LINK','manageReports.do','AC',sysdate);
---1487
-insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Content','Manage Content','API_LINK','manageContent.do','AC',sysdate);
---1488
-insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Reset Password','Reset Password','API_LINK','resetUserPasswordForm.do','AC',sysdate);
---1489
-insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time)
-values(DB_REPORT_ID_SEQ.Nextval,'Manage Education Center Users','Manage Education Center Users','API_LINK','manageEducationCenter.do','IN',sysdate);
+--For TASC
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Users','Manage Users','API_LINK','userModule.do','AC',sysdate,1);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Organizations','Manage Organizations','API_LINK','manageOrganizations.do','AC',sysdate,1);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Parents','Manage Parents','API_LINK','manageParent.do','IN',sysdate,1);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Students','Manage Students','API_LINK','manageStudent.do','AC',sysdate,1);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Reports','Manage Reports','API_LINK','manageReports.do','AC',sysdate,1);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Content','Manage Content','API_LINK','manageContent.do','IN',sysdate,1);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Reset Password','Reset Password','API_LINK','resetUserPasswordForm.do','IN',sysdate,1);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Education Center Users','Manage Education Center Users','API_LINK','manageEducationCenter.do','AC',sysdate,1);
+
+--For INORS
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Users','Manage Users','API_LINK','userModule.do','AC',sysdate,2);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Organizations','Manage Organizations','API_LINK','manageOrganizations.do','AC',sysdate,2);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Parents','Manage Parents','API_LINK','manageParent.do','AC',sysdate,2);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Students','Manage Students','API_LINK','manageStudent.do','AC',sysdate,2);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Reports','Manage Reports','API_LINK','manageReports.do','AC',sysdate,2);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Content','Manage Content','API_LINK','manageContent.do','AC',sysdate,2);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Reset Password','Reset Password','API_LINK','resetUserPasswordForm.do','AC',sysdate,2);
+
+insert into dash_reports(db_reportid,report_name,report_desc,report_type,report_folder_uri,activation_status,created_date_time,projectid)
+values(DB_REPORT_ID_SEQ.Nextval,'Manage Education Center Users','Manage Education Center Users','API_LINK','manageEducationCenter.do','IN',sysdate,2);
