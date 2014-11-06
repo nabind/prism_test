f726661dd034b8d31a3d1da6c4a22a3616196cc1
Amit Dhara <amit.dhara@ctb.com>
2014-11-06 10:10:05 +0000

    Amitabha: test case

--- INORS/Codebase/INORS/prism-parent/src/test/java/com/ctb/prism/parent/dao/ParentDAOImplTest.java
+++ INORS/Codebase/INORS/prism-parent/src/test/java/com/ctb/prism/parent/dao/ParentDAOImplTest.java
@@ -1,7 +1,6 @@
 package com.ctb.prism.parent.dao;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import java.util.HashMap;
 import java.util.List;
@@ -9,7 +8,6 @@ import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +16,6 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -27,7 +24,6 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import com.ctb.prism.login.security.provider.AuthenticatedUser;
 import com.ctb.prism.login.security.tokens.UsernamePasswordAuthenticationToken;
 import com.ctb.prism.login.transferobject.UserTO;
-import com.ctb.prism.parent.transferobject.ParentTO;
 import com.ctb.prism.parent.transferobject.QuestionTO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -65,266 +61,6 @@ public class ParentDAOImplTest extends AbstractTransactionalJUnit4SpringContextT
 		paramMap.put("contractName", "inors");
 		List<QuestionTO> secretQuestionList = parentDao.getSecretQuestions(paramMap);
 		assertEquals(secretQuestionList.size(), 11);
-	}
-
-	@Test
-	@Ignore
-	public void testCheckUserAvailability() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testCheckActiveUserAvailability() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testIsRoleAlreadyTagged() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testValidateIC() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetStudentForIC() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testRegisterUser() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	@Rollback(false)
-	public void testSaveInvitationCodeClaim() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testUpdateInvitationCodeClaimCount() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetChildrenList() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetParentList() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testSearchParent() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testSearchParentAutoComplete() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetStudentList() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testSearchStudentAutoComplete() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testSearchStudent() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testSearchStudentOnRedirect() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetAssessmentList() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testUpdateAssessmentDetails() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testFirstTimeUserLogin() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	public void testManageParentAccountDetails() {
-		ParentTO parentTO = parentDao.manageParentAccountDetails(username);
-		assertNotNull(parentTO);
-		assertEquals(parentTO.getUserId(), 1L);
-	}
-
-	@Test
-	@Ignore
-	public void testGetParentSecretQuestionDetails() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetSecurityQuestionForUser() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testValidateAnswers() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetUserNamesByEmail() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testUpdateUserProfile() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testDeletePasswordHistAnswer() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testCheckInvitationCodeClaim() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testAddInvitationToAccount() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetSchoolOrgId() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testRegenerateActivationCode() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testPopulateGrade() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testPopulateSubtest() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testPopulateObjective() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testAddNewContent() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testLoadManageContent() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetContentForEdit() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testUpdateContent() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testDeleteContent() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testModifyGenericForEdit() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetStudentSubtest() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetArticleTypeDetails() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetArticleDescription() {
-		assertNotNull("Not yet implemented");
-	}
-
-	@Test
-	@Ignore
-	public void testGetGradeSubtestInfo() {
-		assertNotNull("Not yet implemented");
 	}
 
 }
