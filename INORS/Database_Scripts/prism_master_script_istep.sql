0630d2331ca39aed3934ff18dbcbb04dc78635e2
Amit Dhara <amit.dhara@ctb.com>
2014-11-26 12:02:37 +0000

    Joy: Single Code Base - Master script

--- INORS/Database_Scripts/prism_master_script_istep.sql
+++ INORS/Database_Scripts/prism_master_script_istep.sql
@@ -1,2 +1,3 @@
 @\Operation_Schema\Packages\master_package.sql;
+@\Operation_Schema\Procedure\PRC_CONFIGURATION.prc;
 --@\Operation_Schema\InsertScript\master_insert_istep.sql; --Need to run one time. Un-comment the line before running.
--- INORS/Database_Scripts/prism_master_script_tasc.sql
+++ INORS/Database_Scripts/prism_master_script_tasc.sql
@@ -1,1 +1,3 @@
 @\Operation_Schema\Packages\master_package.sql;
+@\Operation_Schema\Procedure\PRC_MSG_TYPEID_CORRECTION.prc;
+@\Operation_Schema\Procedure\PRC_CONFIGURATION.prc;
