a3a20387dfc95864fe9952e7e847d70bfca72992
Amit Dhara <amit.dhara@ctb.com>
2014-12-03 11:25:57 +0000

    Joy: Single Code Base  - DB Scripts

--- INORS/Database_Scripts/prism_master_script_istep.sql
+++ INORS/Database_Scripts/prism_master_script_istep.sql
@@ -1,4 +1,6 @@
 @\Operation_Schema\Packages\master_package.sql;
+@\Operation_Schema\Procedure\PRC_REPORT_CONFIGURATION.prc;
 @\Operation_Schema\Procedure\PRC_CONFIGURATION.prc;
+-- Run PRC_REPORT_CONFIGURATION(1) manually
 -- Run PRC_CONFIGURATION(2) manually
 --@\Operation_Schema\InsertScript\master_insert_istep.sql; --Need to run one time. Un-comment the line before running. Please run it manually
--- INORS/Database_Scripts/prism_master_script_tasc.sql
+++ INORS/Database_Scripts/prism_master_script_tasc.sql
@@ -1,3 +1,5 @@
 @\Operation_Schema\Packages\master_package.sql;
+@\Operation_Schema\Procedure\PRC_REPORT_CONFIGURATION.prc;
 @\Operation_Schema\Procedure\PRC_CONFIGURATION.prc;
+-- Run PRC_REPORT_CONFIGURATION(1) manually
 -- Run PRC_CONFIGURATION(1) manually
