7e12de82583211c753ab06fa25ffd7d9c1667959
Amit Dhara <amit.dhara@ctb.com>
2014-12-11 12:13:00 +0000

    Amitabha: education center admin user role update

--- INORS/Database_Scripts/prism_master_script_tasc.sql
+++ INORS/Database_Scripts/prism_master_script_tasc.sql
@@ -1,5 +1,6 @@
 @\Operation_Schema\Packages\master_package.sql;
 @\Operation_Schema\Procedure\PRC_REPORT_CONFIGURATION.prc;
 @\Operation_Schema\Procedure\PRC_CONFIGURATION.prc;
+@\Operation_Schema\InsertScript\EDU_USER_ROLE_UPDATE.sql
 -- Run PRC_REPORT_CONFIGURATION(1) manually
 -- Run PRC_CONFIGURATION(1) manually
