8fe8342ae89b8d74a50efd3fa3fe4fc509dd55fa
Amit Dhara <amit.dhara@ctb.com>
2014-12-19 13:21:01 +0000

    Amitabha: junit parent controller

--- INORS/Codebase/INORS/prism-web/src/test/java/com/ctb/prism/web/controller/ParentControllerTest.java
+++ INORS/Codebase/INORS/prism-web/src/test/java/com/ctb/prism/web/controller/ParentControllerTest.java
@@ -3,31 +3,27 @@ package com.ctb.prism.web.controller;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextImpl;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ctb.prism.core.constant.IApplicationConstants;
-import com.ctb.prism.login.security.provider.AuthenticatedUser;
-import com.ctb.prism.login.security.tokens.UsernamePasswordAuthenticationToken;
-import com.ctb.prism.login.transferobject.UserTO;
+import com.ctb.prism.test.TestParams;
+import com.ctb.prism.test.TestUtil;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/META-INF/spring/test-context.xml" })
+@ContextConfiguration(locations = { "/test-context.xml" })
 public class ParentControllerTest extends AbstractJUnit4SpringContextTests {
 
 	private MockHttpServletRequest request;
@@ -36,24 +32,22 @@ public class ParentControllerTest extends AbstractJUnit4SpringContextTests {
 	@Autowired
 	private ParentController controller;
 
-	private String username = "ctbadmin";
+	TestParams testParams;
 
 	@Before
 	public void setUp() throws Exception {
+		// Populate test params from properties file
+		testParams = TestUtil.getTestParams();
+
+		// Create request and response for Controller methods
 		request = new MockHttpServletRequest();
-		request.getSession().setAttribute(IApplicationConstants.CURRUSER, username);
 		response = new MockHttpServletResponse();
-		String password = "Passwd12";
-		String contractName = "inors";
-		UserTO user = new UserTO();
-		user.setUserName(username);
-		user.setPassword(password);
-		user.setContractName(contractName);
-		UserDetails targetUser = new AuthenticatedUser(user);
-		Authentication authentication = new UsernamePasswordAuthenticationToken(targetUser, targetUser.getPassword());
-		SecurityContext context = new SecurityContextImpl();
-		context.setAuthentication(authentication);
-		SecurityContextHolder.setContext(context);
+
+		// Set all Session Attributes
+		TestUtil.setSessionAttributes(request, testParams);
+
+		// Bypass login
+		TestUtil.byPassLogin(testParams);
 	}
 
 	@After
@@ -61,11 +55,59 @@ public class ParentControllerTest extends AbstractJUnit4SpringContextTests {
 	}
 
 	@Test
-	@Ignore
-	public void testMyAccount() throws Exception {
+	public void testParentRegistration() throws IOException {
+		ModelAndView mv = controller.parentRegistration(request, response);
+		assertNotNull(mv);
+	}
+
+	@Test
+	public void testAddNewInvitation() throws IOException {
+		ModelAndView mv = controller.addNewInvitation(request, response);
+		assertNotNull(mv);
+	}
+
+	@Test
+	public void testRegister() throws IOException {
+		ModelAndView mv = controller.register(request, response);
+		assertNotNull(mv);
+	}
+
+	@Test
+	public void testMyAccount() throws ServletException, IOException {
 		ModelAndView mv = controller.myAccount(request, response);
 		assertNotNull(mv);
+		System.out.println("testMyAccount.mv.getViewName() = " + mv.getViewName());
 		assertEquals(mv.getViewName(), "user/profile");
+	}
+
+	@Test
+	public void testUpdateProfile() throws IOException {
+		ModelAndView mv = controller.updateProfile(request, response);
+		assertNotNull(mv);
+	}
+
+	@Test
+	public void testValidateCode() throws IOException {
+		String json = controller.validateCode(request, response);
+		assertNotNull(json);
+	}
+
+	@Test
+	public void testCheckAvailability() throws IOException {
+		String json = controller.checkAvailability(request, response);
+		assertNotNull(json);
+	}
+
+	@Test
+	public void testGetChildrenList() {
+		String json = controller.getChildrenList(request, response);
+		assertNotNull(json);
+	}
+
+	@Test
+	public void testValidatePwd() {
+		ModelAndView mv = controller.validatePwd(request, response);
+		assertNotNull(mv);
 	}
 
 }
