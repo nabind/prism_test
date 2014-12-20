b9e56d014c49eb5ec7ac21d34d40e435091be24b
Amit Dhara <amit.dhara@ctb.com>
2014-12-20 00:20:49 +0000

    fix app scan

--- INORS/Codebase/INORS/prism-web/src/main/webapp/scripts/js/admin/login.js
+++ INORS/Codebase/INORS/prism-web/src/main/webapp/scripts/js/admin/login.js
@@ -132,7 +132,7 @@ $(document).ready(function() {
 								var patt=/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i ;
 								if ($("button.submitUser").text()!="OK"){
 									if(	(($("#f_email").val()).replace(/\s/g,"").length==0) || (!patt.test($("#f_email").val())) ){
-										$("#invalidEmailMsg").text("The email id entered is not valid.");
+										$("#invalidEmailMsg").text("The email id format is not valid.");
 										$("#invalidEmailMsg").show(500);
 										$("#mailSentNotofication").hide();
 										
@@ -338,16 +338,19 @@ $(document).ready(function() {
 					if( data.sendEmailFlag == "1") {
 						$("#invalidEmailMsg").hide();
 						$("#mailSentNotofication").show(500);
-						$("#mailSentNotofication").html("<span style=\"color: green\">UserName has been sent Successfully!!</span>");
+						$("#mailSentNotofication").html("<span>UserName has been sent Successfully!! <br/>If you have not received a email, please recheck email id provided or contact the Help Desk.</span>");
 						/*var iscrearted = makeuserDom(data);	
 						if (iscrearted){
 							$("button.submitUser").text("OK");
 						}*/
 						
 						} else {
-							$("#mailSentNotofication").hide();
-							$("#invalidEmailMsg").show(500);	
-							$("#invalidEmailMsg").text("There are no users present with this email id. Please retry with the correct email id.");
+							//$("#mailSentNotofication").hide();
+							//$("#invalidEmailMsg").show(500);	
+							//$("#invalidEmailMsg").text("There are no users present with this email id. Please retry with the correct email id.");
+							// changed as per app scan suggestion
+							$("#mailSentNotofication").show(500);
+							$("#mailSentNotofication").html("<span>UserName has been sent Successfully!! <br/>If you have not received a email, please recheck email id provided or contact the Help Desk.</span>");
 						}					
 													
 				} else {
