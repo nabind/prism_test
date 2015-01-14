65af2f49ed71bfa4f63a71556c31e128d4ba63bb
Abir Dutta <abir.d@tcs.com>
2015-01-14 05:56:56 +0000

    ClearCacheUtility property driven

--- INORS/ClearCacheUtility/src/com/ctb/prism/process/ClearCache.java
+++ INORS/ClearCacheUtility/src/com/ctb/prism/process/ClearCache.java
@@ -10,34 +10,43 @@ import com.ctb.prism.security.DigitalMeasuresHMACQueryStringBuilder;
 
 public class ClearCache {
 
-	private static String REQUEST_URL = "http://10.160.23.12:8080/prism/clearContractCache.do";
-	private static String SECRET_KEY =  "BTCguSF49hYaPmAfe9Q29LtsQ2X";
+	//private static String REQUEST_URL = "http://10.160.23.12:8080/prism/clearContractCache.do";
+	//private static String SECRET_KEY =  "BTCguSF49hYaPmAfe9Q29LtsQ2X";
 	private static String URL_ENCODING = "UTF-8";
 	private static String ENCODING_ALGORITHM = "HmacSHA1";
 	private static String timeZone = "GMT";
+	
+	static ResourceBundle rb = ResourceBundle.getBundle("com.ctb.prism.bundle");
+	
+	private static String REQUEST_URL = "/prism/clearContractCache.do";
 
 
 	
 public static void main(String[] args) throws Exception {
-		String theme = args[0];
-		DigitalMeasuresHMACQueryStringBuilder queryStringBuilder = new DigitalMeasuresHMACQueryStringBuilder(SECRET_KEY, 3600*60, ENCODING_ALGORITHM);
-		queryStringBuilder.setTimeZone(timeZone);
-		queryStringBuilder.setENCODING_ALGORITHM(ENCODING_ALGORITHM);
-		queryStringBuilder.setURL_ENCODING(URL_ENCODING);
-	
-		String requestParam = queryStringBuilder.buildAuthenticatedQueryString(theme);
-		System.out.println(REQUEST_URL + "?" + requestParam);
+		REQUEST_URL = "http://" + rb.getString("server.url") + REQUEST_URL;
+		String SECRET_KEY = rb.getString("hmac.secret.key");
+			
+		if(args.length > 0){
+			String theme = args[0];
+			DigitalMeasuresHMACQueryStringBuilder queryStringBuilder = new DigitalMeasuresHMACQueryStringBuilder(SECRET_KEY, 3600*60, ENCODING_ALGORITHM);
+			queryStringBuilder.setTimeZone(timeZone);
+			queryStringBuilder.setENCODING_ALGORITHM(ENCODING_ALGORITHM);
+			queryStringBuilder.setURL_ENCODING(URL_ENCODING);
 		
-		requestWithQueryString(requestParam);
-		
-	//	invalidRequestWithQueryString(requestParam.replace("validUntil=2013", "validUntil=2012"));
+			String requestParam = queryStringBuilder.buildAuthenticatedQueryString(theme);
+			System.out.println(REQUEST_URL + "?" + requestParam);
+			
+			requestWithQueryString(requestParam);
+		} else {
+			System.out.println("Please provide theme as parameter");
+		}
 	
 	}
 
 
  private static void requestWithQueryString(String queryString) throws IOException
-	{
-		String request =  REQUEST_URL + "?" + queryString;
+	{	
+	 	String request =  REQUEST_URL + "?" + queryString;
 		URL url = new URL(request); 
 	    HttpURLConnection connection = (HttpURLConnection) url.openConnection();           
 	    connection.setDoOutput(true); 
