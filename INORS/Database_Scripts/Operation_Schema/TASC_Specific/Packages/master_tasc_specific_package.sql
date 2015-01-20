99f2edf39af3bd276425ce65fa411e7279bcdae8
Abir Dutta <abir.d@tcs.com>
2015-01-20 05:52:45 +0000

    **Single Codebase** Moved to tasc specific

---
+++ INORS/Database_Scripts/Operation_Schema/TASC_Specific/Packages/master_tasc_specific_package.sql
@@ -0,0 +1,1 @@
+@\PKG_ER_MODULE.pck;
--- INORS/Database_Scripts/prism_master_script_tasc.sql
+++ INORS/Database_Scripts/prism_master_script_tasc.sql
@@ -6,4 +6,5 @@
 @\Operation_Schema\InsertScript\EDU_USER_ROLE_UPDATE.sql;
 @\Operation_Schema\InsertScript\DASH_MENU_RPT_ACCESS_update.sql;
 @\Operation_Schema\TASC_Specific\Functions\master_tasc_specific_functions.sql;
+@\Operation_Schema\TASC_Specific\Packages\master_tasc_specific_package.sql
 @\Operation_Schema\AlterScript\job_tracking_alter.sql;
