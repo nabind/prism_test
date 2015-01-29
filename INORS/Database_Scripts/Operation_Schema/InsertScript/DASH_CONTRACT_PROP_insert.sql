e652e0934acbf4e50bfc2ef860a2a22c6eabf192
Amit Dhara <amit.dhara@ctb.com>
2015-01-29 10:44:19 +0000

    Joy: Single Code Base  - dash_contract_prop insert

--- INORS/Database_Scripts/Operation_Schema/InsertScript/DASH_CONTRACT_PROP_insert.sql
+++ INORS/Database_Scripts/Operation_Schema/InsertScript/DASH_CONTRACT_PROP_insert.sql
@@ -1,5 +1,5 @@
-﻿prompt PL/SQL Developer import file
-prompt Created on Friday, December 05, 2014 by 233208
+prompt PL/SQL Developer import file
+prompt Created on Thursday, January 29, 2015 by 541841
 set feedback off
 set define off
 prompt Disabling triggers for DASH_CONTRACT_PROP...
@@ -11,53 +11,61 @@ delete from DASH_CONTRACT_PROP;
 commit;
 prompt Loading DASH_CONTRACT_PROP...
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (190, 'role.not.added', 'ROLE_CTB,ROLE_PARENT,ROLE_SUPER,ROLE_EDU_ADMIN', null, 2);
+values (190, 'static.pdf.location', 'INORSREPORTS', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (191, 'orglvl.user.not.added', '0', null, 2);
+values (191, 'static.pdf.location', 'TASCREPORTS', null, 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (192, 'orglvl.admin.not.added', '4', null, 2);
+values (192, 'sso.redirect.loginfail', 'https://renqa.ctb.com/ctb.com/control/main', 'CTB.COM|inors', 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (193, 'hmac.secret.key', 'BTCguSF49hYaPmAfe9Q29LtsQ2X', 'CTB.COM|inors', 2);
+values (193, 'sso.redirect.loginfail', 'https://oastest.ctb.com/SessionWeb/login.jsp', 'OAS|tasc', 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (194, 'aws.inors.cacheS3', 'Cache_Keys/INORS/', null, 2);
+values (194, 'sso.redirect.logout', 'https://renqa.ctb.com/ctb.com/control/main', 'CTB.COM|inors', 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (195, 'password.history.day', '3', null, 2);
+values (195, 'sso.redirect.logout', 'https://oastest.ctb.com/SessionWeb/login.jsp', 'OAS|tasc', 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (196, 'title.tab.home.application', 'Indiana Online Reporting System', null, 2);
+values (131, 'role.not.added', 'ROLE_CTB,ROLE_PARENT,ROLE_SUPER,ROLE_EDU_ADMIN', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (197, 'title.tab.application', 'Indiana Online Reporting System', null, 2);
+values (132, 'orglvl.user.not.added', '0', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (198, 'password.expiry', '90', null, 2);
+values (133, 'orglvl.admin.not.added', '4', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (199, 'password.expiry.warning', '85', null, 2);
+values (134, 'hmac.secret.key', 'BTCguSF49hYaPmAfe9Q29LtsQ2X', 'CTB.com|inors', 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (200, 'static.pdf.location', 'INORSREPORTS', null, 2);
+values (135, 'aws.inors.cacheS3', 'Cache_Keys/INORS/', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (201, 'orglvl.user.not.added', '0', null, 1);
+values (162, 'password.history.day', '3', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (202, 'orglvl.admin.not.added', '4', null, 1);
+values (182, 'title.tab.home.application', 'Indiana Online Reporting System', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (203, 'aws.inors.cacheS3', 'Cache_Keys/TASC/', null, 1);
+values (164, 'title.tab.application', 'Indiana Online Reporting System', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (204, 'hmac.secret.key', 'WPZguVF49hXaRuZfe9L29ItsC2I', 'OAS|tasc', 1);
+values (184, 'password.expiry', '90', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (205, 'hmac.secret.key', 'ETCguRF49hEaRuZguVF49hXrc1', 'eResource|tasc', 1);
+values (186, 'password.expiry.warning', '85', null, 2);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (206, 'role.not.added', 'ROLE_CTB,ROLE_PARENT,ROLE_SUPER,ROLE_EDU_ADMIN', null, 1);
+values (137, 'orglvl.user.not.added', '0', null, 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (207, 'password.history.day', '3', null, 1);
+values (138, 'orglvl.admin.not.added', '4', null, 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (208, 'title.tab.home.application', 'TASC Dataonline', null, 1);
+values (139, 'aws.inors.cacheS3', 'Cache_Keys/TASC/', null, 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (209, 'title.tab.application', 'TASC-Login', null, 1);
+values (140, 'hmac.secret.key', 'WPZguVF49hXaRuZfe9L29ItsC2I', 'OAS|tasc', 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (210, 'password.expiry', '90', null, 1);
+values (141, 'hmac.secret.key', 'RPEgeVS49XaUu29ItsC2IoZre9C', 'ERESOURCE|tasc', 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (211, 'password.expiry.warning', '85', null, 1);
+values (136, 'role.not.added', 'ROLE_CTB,ROLE_PARENT,ROLE_SUPER,ROLE_EDU_ADMIN', null, 1);
 insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
-values (212, 'static.pdf.location', 'TASCREPORTS', null, 1);
+values (161, 'password.history.day', '3', null, 1);
+insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
+values (181, 'title.tab.home.application', 'TASC Dataonline', null, 1);
+insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
+values (163, 'title.tab.application', 'TASC-Login', null, 1);
+insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
+values (183, 'password.expiry', '90', null, 1);
+insert into DASH_CONTRACT_PROP (DB_PROPERTYID, DB_PROPERTY_NAME, DB_PROPERY_VALUE, SSO_SOURCE, PROJECTID)
+values (185, 'password.expiry.warning', '85', null, 1);
 commit;
-prompt 23 records loaded
+prompt 27 records loaded
 prompt Enabling foreign key constraints for DASH_CONTRACT_PROP...
 alter table DASH_CONTRACT_PROP enable constraint FK_DASH_CONTRACT_PROP;
 prompt Enabling triggers for DASH_CONTRACT_PROP...
--- INORS/Database_Scripts/Operation_Schema/InsertScript/master_insert.sql
+++ INORS/Database_Scripts/Operation_Schema/InsertScript/master_insert.sql
@@ -6,6 +6,7 @@
 @\dash_message_type_enable_constraint.sql -- delete the irrelevant data from dash_message_type first
 @\DASH_REPORT_insert.sql;
 @\DASH_REPORT_ACTION_insert.sql;
+@\DASH_CONTRACT_PROP_insert.sql;
 @\gscm_tasc.sql;
 @\customer_info_update.sql;
 @\adhoc_dml.sql;
@@ -15,3 +16,24 @@
 --Need to run environment wise (eg: performence = perf)
 --update dash_contract_prop set db_propery_value  = 'ISTEPREPORTS/perf' where db_property_name  = 'static.pdf.location' and projectid = 2;
 --update dash_contract_prop set db_propery_value = 'TASCREPORTS/perf' where db_property_name = 'static.pdf.location' and projectid = 1;
+/*
+update PRISMGLOBAL.DASH_CONTRACT_PROP
+   set db_propery_value = 'https://renqa.ctb.com/ctb.com/control/main'
+ where projectid = 2
+   and db_property_name = 'sso.redirect.logout';
+
+update PRISMGLOBAL.DASH_CONTRACT_PROP
+   set db_propery_value = 'https://renqa.ctb.com/ctb.com/control/main'
+ where projectid = 2
+   and db_property_name = 'sso.redirect.loginfail';
+
+update PRISMGLOBAL.DASH_CONTRACT_PROP
+   set db_propery_value = 'https://oastest.ctb.com/SessionWeb/login.jsp'
+ where projectid = 1
+   and db_property_name = 'sso.redirect.logout';
+
+update PRISMGLOBAL.DASH_CONTRACT_PROP
+   set db_propery_value = 'https://oastest.ctb.com/SessionWeb/login.jsp'
+ where projectid = 1
+   and db_property_name = 'sso.redirect.loginfail';
+*/
