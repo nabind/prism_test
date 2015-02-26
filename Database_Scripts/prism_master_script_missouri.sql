b3358a3550a011ed158dd3664405a91be34e5749
Amit Dhara <amit.dhara@ctb.com>
2015-02-26 12:54:53 +0000

    Joy: Manage Content for MO

--- Database_Scripts/prism_master_script_missouri.sql
+++ Database_Scripts/prism_master_script_missouri.sql
@@ -1,3 +1,5 @@
+@\Operation_Schema\Packages\master_function.sql;
+@\Operation_Schema\Packages\master_package.sql;
 @\Operation_Schema\MISSOURI_Specific\Types\master_missouri_specific_types.sql;
 @\Operation_Schema\MISSOURI_Specific\Functions\master_missouri_specific_functions.sql;
 @\Operation_Schema\MISSOURI_Specific\Packages\master_missouri_specific_packages.sql;
