a7c0df3ea96f779eddba06bf6e4c6380959b5e6f
Abir Dutta <abir.d@tcs.com>
2015-02-26 16:33:38 +0000

    *** empty log message ***

---
+++ INORS/Database_Scripts/Operation_Schema/SCB_CODE_MIGRATION_SCRIPTS/TRANSACTIONAL_ISTEP/ISTEP_SPECIFIC/Function/MASTESR_FUNCTION_SCRIPT_INORS_SPECIFIC.sql
@@ -0,0 +1,5 @@
+@\SF_GEN_INVITE_CODE.fnc;
+@\sf_get_list_of_roles.fnc;
+@\SF_ORG_USER_ID_GEN.fnc;
+@\SF_STG_BIO_DETAILS_ID.fnc;
+@\SF_USER_ID_GEN.fnc;
---
+++ INORS/Database_Scripts/Operation_Schema/SCB_CODE_MIGRATION_SCRIPTS/TRANSACTIONAL_ISTEP/ISTEP_SPECIFIC/Package/MASTER_INORS_PACKAGE.sql
@@ -0,0 +1,5 @@
+@\PKG_DELETE_FACT_TABLES.pck;
+@\PKG_INORS_MIGRATION.pck;
+@\PKG_ISTEP_MATRIX.pck;
+@\pkg_report_acad_stand_summary.pck;
+@\PRISM_REPORT.pck;
