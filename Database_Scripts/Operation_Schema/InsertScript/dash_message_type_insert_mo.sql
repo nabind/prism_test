6d08777dcdb566ad89e4ce239505acb1e3d1360a
Amit Dhara <amit.dhara@ctb.com>
2015-03-03 15:03:08 +0000

    Joy: Group Downloads for MO

---
+++ Database_Scripts/Operation_Schema/InsertScript/dash_message_type_insert_mo.sql
@@ -0,0 +1,44 @@
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+     cust_prod_id,
+       created_date_time)
+    values
+      (1085,
+       'Group Download Instruction',
+       'GSCM',
+       'Instructions message configuration for Group Download page',
+	   5027,
+       sysdate);
+	   
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+     cust_prod_id,
+       created_date_time)
+    values
+      (1101,
+       'GDF Header Message',
+       'GSCM',
+       'GDF Header Message',
+	   5027,
+       sysdate);
+
+insert into dash_message_type
+      (msg_typeid,
+       message_name,
+       message_type,
+       description,
+     cust_prod_id,
+       created_date_time)
+    values
+      (1102,
+       'GDF Notice',
+       'GSCM',
+       'GDF Notice',
+	   5027,
+       sysdate);	   
--- Database_Scripts/prism_master_script.sql
+++ Database_Scripts/prism_master_script.sql
@@ -1,3 +1,5 @@
-@\Operation_Schema\Procedure\PRC_MSG_TYPEID_CORRECTION.prc;
+--@\Operation_Schema\Procedure\PRC_MSG_TYPEID_CORRECTION.prc;
 --@\Operation_Schema\Packages\PKG_TASC_customer_creation.pck; --Need not to compile
 --@\Operation_Schema\InsertScript\master_insert.sql; --Need to run one time. Un-comment the line before running
+--MO
+@\Operation_Schema\InsertScript\dash_message_type_insert_mo.sql;
