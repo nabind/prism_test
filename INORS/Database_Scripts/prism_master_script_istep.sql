4504f91d4389c4a73f05b43620abee70bbb104de
Amit Dhara <amit.dhara@ctb.com>
2015-03-18 10:47:21 +0000

    Joy: DB Scripts for SCB

--- INORS/Database_Scripts/prism_master_script_istep.sql
+++ INORS/Database_Scripts/prism_master_script_istep.sql
@@ -4,7 +4,7 @@
 @\Operation_Schema\Procedure\PRC_CONFIGURATION.prc;
 -- Run PRC_REPORT_CONFIGURATION(2) manually
 -- Run PRC_CONFIGURATION(2) manually
---@\Operation_Schema\INORS_Specific\Materialized_Views\master_inors_specific_mviews.sql; -- not needed
-@\Operation_Schema\INORS_Specific\Functions\master_inors_specific_functions.sql;
-@\Operation_Schema\INORS_Specific\Packages\master_inors_specific_package.sql;
+--@\Operation_Schema\INORS_Specific\Materialized_Views\master_inors_specific_mviews.sql; -- [Moved to migration script. No need to run]
+--@\Operation_Schema\INORS_Specific\Functions\master_inors_specific_functions.sql; -- [Moved to migration script. No need to run]
+@\Operation_Schema\INORS_Specific\Packages\master_inors_specific_package.sql; 
 @\Operation_Schema\AlterScript\job_tracking_alter.sql;
