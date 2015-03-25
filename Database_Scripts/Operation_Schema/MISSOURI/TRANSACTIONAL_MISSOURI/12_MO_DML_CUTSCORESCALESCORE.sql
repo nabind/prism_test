229da894c3ed5d512a42a037901aabd0fd0d6487
Amit Dhara <amit.dhara@ctb.com>
2015-03-25 17:20:14 +0000

    *** empty log message ***

---
+++ Database_Scripts/Operation_Schema/MISSOURI/TRANSACTIONAL_MISSOURI/12_MO_DML_CUTSCORESCALESCORE.sql
@@ -0,0 +1,122 @@
+--change the cust_prod_id 
+insert into cutscorescalescore values(cutscore_seq.nextval,10007,20007,2054,5027,2114,2623,2114,2366,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10007,20007,2054,5027,2114,2623,2367,2431,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10007,20007,2054,5027,2114,2623,2432,2489,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10007,20007,2054,5027,2114,2623,2490,2623,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10008,20008,2054,5027,2131,2663,2131,2415,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10008,20008,2054,5027,2131,2663,2416,2472,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10008,20008,2054,5027,2131,2663,2473,2532,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10008,20008,2054,5027,2131,2663,2533,2663,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2054,5027,2201,2701,2201,2441,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2054,5027,2201,2701,2442,2501,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2054,5027,2201,2701,2502,2581,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2054,5027,2201,2701,2582,2701,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10010,20010,2054,5027,2210,2724,2210,2456,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10010,20010,2054,5027,2210,2724,2457,2530,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10010,20010,2054,5027,2210,2724,2531,2617,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10010,20010,2054,5027,2210,2724,2618,2724,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10011,20011,2054,5027,2258,2745,2258,2478,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10011,20011,2054,5027,2258,2745,2479,2551,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10011,20011,2054,5027,2258,2745,2552,2648,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10011,20011,2054,5027,2258,2745,2649,2745,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2054,5027,2288,2769,2288,2486,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2054,5027,2288,2769,2487,2566,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2054,5027,2288,2769,2567,2667,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2054,5027,2288,2769,2668,2769,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10007,20007,2055,5027,2189,2621,2189,2380,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10007,20007,2055,5027,2189,2621,2381,2435,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10007,20007,2055,5027,2189,2621,2436,2500,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10007,20007,2055,5027,2189,2621,2501,2621,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10008,20008,2055,5027,2204,2659,2204,2410,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10008,20008,2055,5027,2204,2659,2411,2484,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10008,20008,2055,5027,2204,2659,2485,2548,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10008,20008,2055,5027,2204,2659,2549,2659,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2055,5027,2219,2700,2219,2454,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2055,5027,2219,2700,2455,2527,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2055,5027,2219,2700,2528,2578,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2055,5027,2219,2700,2579,2700,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10010,20010,2055,5027,2235,2748,2235,2472,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10010,20010,2055,5027,2235,2748,2473,2551,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10010,20010,2055,5027,2235,2748,2552,2609,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10010,20010,2055,5027,2235,2748,2610,2748,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10011,20011,2055,5027,2250,2778,2250,2483,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10011,20011,2055,5027,2250,2778,2484,2566,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10011,20011,2055,5027,2250,2778,2567,2634,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10011,20011,2055,5027,2250,2778,2635,2778,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2055,5027,2265,2802,2265,2503,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2055,5027,2265,2802,2504,2585,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2055,5027,2265,2802,2586,2652,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2055,5027,2265,2802,2653,2802,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2056,5027,470,855,470,625,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2056,5027,470,855,626,668,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2056,5027,470,855,669,691,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10009,20009,2056,5027,470,855,692,855,4,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2056,5027,540,895,540,670,1,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2056,5027,540,895,671,702,2,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2056,5027,540,895,703,734,3,SYSDATE,SYSDATE);
+
+insert into cutscorescalescore values(cutscore_seq.nextval,10012,20012,2056,5027,540,895,735,895,4,SYSDATE,SYSDATE);
+
+COMMIT;
+
+
+
+
+
+
+
+
