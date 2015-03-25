595bec94b4081a4f87c53dcaeebfdafb9e8a8bf0
Amit Dhara <amit.dhara@ctb.com>
2015-03-25 17:27:57 +0000

    *** empty log message ***

--- Database_Scripts/prism_master_script_missouri.sql
+++ Database_Scripts/prism_master_script_missouri.sql
@@ -1,7 +1,8 @@
 @\Operation_Schema\Procedure\PRC_CONFIGURATION.prc;
 -- Run PRC_CONFIGURATION(3) manually
 @\Operation_Schema\Packages\master_package.sql;
-@\Operation_Schema\MISSOURI_Specific\Packages\master_missouri_specific_packages.sql;
-@\Operation_Schema\MISSOURI_Specific\Types\master_missouri_specific_types.sql;
-@\Operation_Schema\MISSOURI_Specific\Functions\master_missouri_specific_functions.sql;
-@\Operation_Schema\MISSOURI_Specific\Materialized_Views\master_missouri_specific_mviews.sql;
+---COMMENTED BY PARTHA
+--@\Operation_Schema\MISSOURI_Specific\Packages\master_missouri_specific_packages.sql; 
+--@\Operation_Schema\MISSOURI_Specific\Types\master_missouri_specific_types.sql;
+--@\Operation_Schema\MISSOURI_Specific\Functions\master_missouri_specific_functions.sql;
+--@\Operation_Schema\MISSOURI_Specific\Materialized_Views\master_missouri_specific_mviews.sql;
