e4ce19cffc99378437242d5061687e08da3c8e19
Abir Dutta <abir.d@tcs.com>
2015-05-18 14:02:31 +0000

    Junit

--- Codebase/SCB/prism-core/src/test/java/com/ctb/prism/core/Service/RepositoryServiceImplTest.java
+++ Codebase/SCB/prism-core/src/test/java/com/ctb/prism/core/Service/RepositoryServiceImplTest.java
@@ -6,13 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/test-context.xml" })
 @Ignore
 public class RepositoryServiceImplTest  extends AbstractJUnit4SpringContextTests  {
 
