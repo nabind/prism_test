df2260b9deb0c693fb1ea0681d4d772b03bdf7e2
Abir Dutta <abir.d@tcs.com>
2015-05-18 13:49:11 +0000

    Junit

---
+++ Codebase/SCB/prism-core/src/test/java/com/ctb/prism/core/Service/RepositoryServiceImplTest.java
@@ -0,0 +1,92 @@
+package com.ctb.prism.core.Service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/test-context.xml" })
+@Ignore
+public class RepositoryServiceImplTest  extends AbstractJUnit4SpringContextTests  {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSetS3Client() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBucket() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAssetList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAssetListWithPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAssetBytes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAssetBytesByS3Key() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUploadAssetStringFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUploadMapAsset() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUploadAssetStringInputStream() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUploadAssetByS3Key() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveAsset() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAssetInputStream() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUploadAssetAsync() {
+		fail("Not yet implemented");
+	}
+
+}
