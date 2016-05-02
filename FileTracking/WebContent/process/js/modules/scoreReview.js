46d47cbdb57a439cd72087596c96cf3224285d37
adutta <>
2016-05-02 13:41:54 +0000

    Update with review result

--- FileTracking/WebContent/process/js/modules/scoreReview.js
+++ FileTracking/WebContent/process/js/modules/scoreReview.js
@@ -79,19 +79,62 @@ function validateForm(){
 }
 
 function clearMoreInfoTableRows() {
-	$("#imagingId").html('');
-	$("#orgTpName").html('');
-	$("#lastName").html('');
-	$("#firstName").html('');
-	$("#middleInitial").html('');
-	$("#lithoCode").html('');
-	$("#scanStack").html('');
-	$("#scanSequence").html('');
-	$("#winsDocId").html('');
-	$("#comodityCode").html('');
-	$("#winStatus").html('');
-	$("#prismProcessStatus").html('');
-	$("#imageFilePath").html('');
-	$("#imageFileName").html('');
+	$("#studentTestId").html('');
+	$("#formName").html('');
+	$("#newNC").html('');
+	$("#newSS").html('');
+	$("#newHSE").html('');
+	$("#processedDate").html('');
+	
 }
 
+function studentDetails(studentTestEventId,subTestName){
+	clearMoreInfoTableRows();
+	var dataString = "studentTestEventId="+studentTestEventId+"&subTestName="+subTestName;
+	$("#review").html( '<img src="css/ajax-loader.gif"></img>' );
+	jQuery("#reviewDialog").dialog({
+		title: 'Student Score Info: ',
+		width: 675,
+		height: 410,
+		draggable: false
+	});
+	$.ajax({
+	      type: "POST",
+	      url: "getStudentScoreInfo.htm",
+	      cache: false,
+	      data: dataString,
+	      success: function(data) {
+	    	  if(data == "Error") {
+	    		  clearMoreInfoTableWhenError('Failed to get Data');
+	    	  } else if(data.length == 2) {
+	    		  clearMoreInfoTableWhenError('Data Not Found');
+	          } else {
+	    		  $("#review").html('');
+		    	  var obj = JSON.parse(data);
+		    	  $("#studentTestId").html( obj.TEST_ELEMENT_ID );
+		    	  $("#formName").html( obj.FORM_NAME );
+		    	  $("#newNC").html( obj.NCE );
+		    	  $("#newSS").html( obj.SS );
+		    	  $("#newHSE").html( obj.HSE );
+		    	  $("#processedDate").html(obj.UPDATED_DATE_TIME);
+		     }
+	      },
+		  error: function(data) {
+			  clearMoreInfoTableWhenError('Failed to get Data');
+		  }
+    });
+
+}
+
+function rejectOther(id){
+	var elem = document.getElementsByName("isApprove");
+	for(var i=1;i<=elem.length;i++){
+	if(i==id){
+		document.getElementById(i).disabled = false;
+    }
+    else 
+    {	
+    	document.getElementById(i).disabled = true;
+    }
+    }
+}
